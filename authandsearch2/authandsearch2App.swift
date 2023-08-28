5cf60a1077ae1e70a3e3b544719a66b965371ea2
Carl Rudling <carl@rudling.eu>
2023-08-28 09:16:45 -1000

    Initial Commit

---
+++ authandsearch2/authandsearch2App.swift
@@ -0,0 +1,17 @@
+//
+//  authandsearch2App.swift
+//  authandsearch2
+//
+//  Created by Carl Rudling on 2023-08-28.
+//
+
+import SwiftUI
+
+@main
+struct authandsearch2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
