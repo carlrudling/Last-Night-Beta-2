ee7d514f5789cee9346d009ff3b63a2554ac9155
Carl Rudling <carl@rudling.eu>
2023-12-05 10:59:46 -1000

    Changed the UI of the chat a bit

--- authandsearch2/Views/Album/MessagesView.swift
+++ authandsearch2/Views/Album/MessagesView.swift
@@ -4,42 +4,60 @@ struct MessagesView: View {
     @StateObject var messagesService: MessagesService
     @State private var albumName: String
     @EnvironmentObject var userService: UserService
-
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    
     init(albumID: String, albumName: String) {
-           _messagesService = StateObject(wrappedValue: MessagesService(albumID: albumID))
-           self.albumName = albumName
-       }
-
+        _messagesService = StateObject(wrappedValue: MessagesService(albumID: albumID))
+        self._albumName = State(initialValue: albumName) // Correct initialization of albumName
+    }
+    
     var body: some View {
         VStack {
             VStack {
-                TitleRow(name: albumName)
-                
                 ScrollViewReader { proxy in
                     ScrollView {
                         ForEach(messagesService.messages, id: \.id) { message in
                             MessageBubble(
-                                  message: message,
-                                  isCurrentUser: message.senderID == userService.uuid // You need to provide the current user's ID here
-                              )
+                                message: message,
+                                isCurrentUser: message.senderID == userService.uuid // You need to provide the current user's ID here
+                            )
                         }
                     }
                     .padding(.top, 10)
                     .background(.white)
-                .cornerRadius(30, corners: [.topLeft, .topRight])
-                .onChange(of: messagesService.lastMessageId) { id in
-                    withAnimation{
-                        proxy.scrollTo(id, anchor: .bottom)
+                    .cornerRadius(30, corners: [.topLeft, .topRight])
+                    .onChange(of: messagesService.lastMessageId) { id in
+                        withAnimation{
+                            proxy.scrollTo(id, anchor: .bottom)
+                        }
                     }
-                }
                 }
             }
             .background(Color(.purple))
             
             MessageField(messagesService: messagesService)
-
-
+            
+            
         }
+        .navigationBarBackButtonHidden(true)
+        .navigationBarTitleDisplayMode(.inline) // Set the display mode to inline
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                HStack {
+                    Button(action: { self.presentationMode.wrappedValue.dismiss() }) {
+                        Image(systemName: "chevron.backward") // Back icon
+                    }
+                    .foregroundColor(.white)
+                    .padding(12)
+                    
+                    Text(albumName) // Album name next to back button
+                        .font(.headline)
+                        .foregroundColor(.white)
+                }
+            }
+        }
+        
     }
 }
 
@@ -48,3 +66,5 @@ struct MessagesView_Previews: PreviewProvider {
         MessagesView(albumID: "dummyAlbumID", albumName: "Dummy Album Name")
     }
 }
+
+
